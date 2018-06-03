99651be530cb3eb54ba044c8bcf35c2ed119d8c4
Hoixi <furkan.kayali.1905@gmail.com>
2018-06-03 17:51:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import random
 from discord.ext.commands import Bot
 from discord.ext import commands
 import os
+from random import *
 
 
 
@@ -111,8 +112,10 @@ async def soyle(ctx):
     print(ctx.message.author.name, msg)
     
 
-
-
+@bot.command(pass_context=True)
+async def kaçcm(ctx):
+    x = randint(1, 35) 
+    await bot.say(ctx.message.author.name + "'nin " + str(x) + " cm yarrağı var")
 
 
     
